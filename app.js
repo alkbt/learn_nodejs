4895d20a9a4b9c5c62e0b057566272293baa45b2
Aleksey Kourbatov <alkbt@yandex.ru>
2018-06-12 23:08:55 +0300

    Простой рендеринг записей из MongoDb.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require("express"),
+    mongoose = require("mongoose"),
     app = express();
 
+app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/test");
+
+var Records = mongoose.model("Record", {
+    title: String,
+    message: String
+});
+
 app.get("/", function (req, res) {
-    res.send("Hello, World!");
+    Records.find({}, function (err, records) {
+        if (err) {
+            console.log("Records.find error " + err);
+            return;
+        }
+
+        res.render("main", {records: records});
+    });
 });
 
 app.listen(3000, () => {
